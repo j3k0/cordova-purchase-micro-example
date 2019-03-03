2ffb079d35d609ce3b4eddea3645d55532b69412
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-03-03 09:29:09 +0200

    Update to latest plugin version + expiryDate

--- www/js/index.js
+++ www/js/index.js
@@ -14,7 +14,7 @@ function onDeviceReady() {
     }]);
 
     // For subscriptions and secured transactions, we setup a receipt validator.
-    store.validator = "https://billing.fovea.cc/v1/validate?appName=test&apiKey=13d71c00-e703-49d0-b354-3d989bbfe865";
+    store.validator = "https://devbox-reeceipt-validator.fovea.cc/v1/validate?appName=test&apiKey=13d71c00-e703-49d0-b354-3d989bbfe865";
 
     // Show errors on the dedicated Div.
     store.error(function(error) {
@@ -72,14 +72,16 @@ function renderUI() {
 
         // Create and update the HTML content
         const info = product.loaded
-            ? `title: ${product.title}<br/>` +
-              `desc: ${product.description}<br/>` +
-              `price: ${product.price}<br/>` +
-              `state: ${product.state}<br/>`
-            : '...';
+            ? `title:      ${ product.title       || '' }<br/>` +
+              `desc:       ${ product.description || '' }<br/>` +
+              `price:      ${ product.price       || '' }<br/>` +
+              `state:      ${ product.state       || '' }<br/>` +
+              `expiry:     ${ product.expiryDate  || '' }<br/>`
+            : `...`;
         const button = product.canPurchase
             ? `<button style="margin:20px 0" onclick="store.order('${product.id}')">Buy Now!</button>`
             : '';
         document.getElementById(`${alias}-purchase`).innerHTML = info + button;
     }
 }
+
