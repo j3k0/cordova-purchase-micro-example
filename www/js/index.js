08034c31e5f120d8d47d2cb6b948488dbefc6199
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-05-29 13:42:40 +0300

    Update code for using a non-consumable

--- www/js/index.js
+++ www/js/index.js
@@ -4,15 +4,12 @@ function onDeviceReady() {
 
     // We should first register all our products or we cannot use them in the app.
     store.register([{
-        id:    'subscription1',
-        type:   store.PAID_SUBSCRIPTION,
-    }, {
-        id:    'subscription2',
-        type:   store.PAID_SUBSCRIPTION,
+        id:    'nonconsumable1',
+        type:   store.NON_CONSUMABLE,
     }]);
     store.verbosity = store.DEBUG;
 
-    // For subscriptions and secured transactions, we setup a receipt validator.
+    // For secured transactions, we setup a receipt validator.
     store.validator = "https://reeceipt-validator.fovea.cc/v1/validate?appName=test&apiKey=13d71c00-e703-49d0-b354-3d989bbfe865";
 
     // Show errors on the dedicated Div.
@@ -24,7 +21,7 @@ function onDeviceReady() {
     });
 
     // Define events handler for our subscription products
-    store.when('subscription')
+    store.when('product')
          .updated(renderUI)          // render the interface on updates
          .approved(p => p.verify())   // verify approved transactions
          .verified(p => p.finish());  // finish verified transactions
@@ -39,27 +36,22 @@ function onDeviceReady() {
 // Perform a full render of the user interface
 function renderUI() {
 
-    // When either of our susbscription products is owned, display "Subscribed".
-    // If one of them is being purchased or validated, display "Processing".
-    // In all other cases, display "Not Subscribed".
+    // When either of our products is owned, display "Full Version Unlocked".
+    // If it is being purchased or validated, display "Processing".
+    // In all other cases, display "Free Version".
     if (haveState('owned'))
-        document.getElementById('status').textContent = 'Subscribed';
+        document.getElementById('status').textContent = 'Full Version Unlocked';
     else if (haveState('approved') || haveState('initiated'))
         document.getElementById('status').textContent = 'Processing...';
     else
-        document.getElementById('status').textContent = 'Not Subscribed';
+        document.getElementById('status').textContent = 'Free Version';
 
-    // Render the products' DOM elements "subscription1-purchase" and "subscription2-purchase"
-    renderProductUI('subscription1');
-    renderProductUI('subscription2');
+    // Render the products' DOM elements
+    renderProductUI('nonconsumable1');
 
     // Does any our product has the given state?
     function haveState(value) {
-        return getState('subscription1') === value || getState('subscription2') === value;
-
-        function getState(id) {
-            return store.get(id) ? store.get(id).state : '';
-        }
+        return store.get('nonconsumable1') && store.get('nonconsumable1').state === value;
     }
 
     // Refresh the displayed details about a product in the DOM
@@ -75,7 +67,6 @@ function renderUI() {
               `desc:   ${ product.description || '' }<br/>` +
               `price:  ${ product.price       || '' }<br/>` +
               `state:  ${ product.state       || '' }<br/>` +
-              `expiry: ${ product.expiryDate && product.expiryDate.toString() || '' }<br/>`
             : `...`;
         const button = product.canPurchase
             ? `<button style="margin:20px 0" onclick="store.order('${product.id}')">Buy Now!</button>`
