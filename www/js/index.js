91dd1cecbb6ac81f3dcd1286d16313f08a35cadd
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-09-20 21:11:16 +0300

    Make use of discount offers

--- www/js/index.js
+++ www/js/index.js
@@ -2,11 +2,15 @@ document.addEventListener('deviceready', onDeviceReady);
 
 function onDeviceReady() {
 
-    // We should first register all our products or we cannot use them in the app.
-    store.register([{
-        id:    'subscription1',
-        type:   store.PAID_SUBSCRIPTION,
+    const products = [{
+        id:    'cc.fovea.purchase.subscription1sx',
+        alias: 'sub1',
+        type:  store.PAID_SUBSCRIPTION,
     }, {
+        id:    'cc.fovea.purchase.subscription2sx',
+        alias: 'sub2',
+        type:  store.PAID_SUBSCRIPTION,
+    }/*, {
         id:    'nonconsumable1',
         type:   store.NON_CONSUMABLE,
     }, {
@@ -18,25 +22,19 @@ function onDeviceReady() {
     }, {
         id:    'subscription2',
         type:   store.PAID_SUBSCRIPTION,
-    }]);
+    }*/];
+
+    // We should first register all our products or we cannot use them in the app.
+    store.register(products);
     store.verbosity = store.DEBUG;
+    store.applicationUsername = "hellokitty"; // plugin will hash this with md5 when needed
 
     // For subscriptions and secured transactions, we setup a receipt validator.
-    store.validator = "https://reeceipt-validator.fovea.cc/v1/validate?appName=test&apiKey=13d71c00-e703-49d0-b354-3d989bbfe865";
+    store.validator = "https://devbox-reeceipt-validator.fovea.cc/v1/validate?appName=demosx&apiKey=3a45c2cf-d213-427c-b5bb-1ead3a61332b";
+    // store.validator = "https://validator.fovea.cc/v1/validate?appName=test1&apiKey=d38078ab-154b-42ae-9042-20453c8e21e1";
 
     // Show errors on the dedicated Div.
-    store.error(function(error) {
-      document.getElementById('error').textContent = `ERROR ${error.code}: ${error.message}`;
-      setTimeout(() => {
-        document.getElementById('error').innerHTML = '<br/>';
-      }, 10000);
-      if (error.code === store.ERR_LOAD_RECEIPTS) {
-        // Cannot load receipt, ask user to refresh purchases.
-        setTimeout(() => {
-          alert('Cannot access purchase information. Use "Refresh" to try again.');
-        }, 1);
-      }
-    });
+    store.error(errorHandler);
 
     // Define events handler for our subscription products
     store.when()
@@ -110,9 +108,14 @@ function renderUI() {
         const introPrice = product.loaded && product.type === store.PAID_SUBSCRIPTION && product.introPrice
             ? `intro:  ${ strikeIf(product.ineligibleForIntroPrice) }${ product.introPrice || '' } for ${ product.introPricePeriod || '' } ${ product.introPricePeriodUnit || '' } (${ product.introPricePaymentMode || '' })${ strikeEnd(product.ineligibleForIntroPrice) }<br/>`
             : '';
+        function discountButton(discount) {
+          return discount.eligible
+            ? `<button onclick="orderDiscount('${ product.id }', '${ discount.id }')">Redeem</button>`
+            : '';
+        }
         const discounts = product.discounts && product.discounts.map(discount =>
               `discount: ${ strikeIf(!discount.eligible) }${ discount.type } ${ discount.paymentMode }` +
-              ` - ${ discount.price } for ${ discount.period } ${ discount.periodUnit }${ strikeEnd(!discount.eligible) }<br/>`).join('') || '';
+              ` - ${ discount.price } for ${ discount.period } ${ discount.periodUnit }${ discountButton(discount) }${ strikeEnd(!discount.eligible) }<br/>`).join('') || '';
         const subInfo = product.loaded && product.type === store.PAID_SUBSCRIPTION
             ? `per:    ${ product.billingPeriod || '' } ${ product.billingPeriodUnit || '' }<br/>` +
               (product.expiryDate ? `expiry: ${ product.expiryDate.toString() || '' }<br/>` : '') +
@@ -120,9 +123,73 @@ function renderUI() {
               (product.group ? `group:  ${ product.group }<br />` : '')
             : '';
         const button = product.canPurchase
-            ? `<button style="margin:20px 0" onclick="store.order('${product.id}', {applicationUsername: 'hellokitty'})">Buy Now!</button>`
+            ? `<button style="margin:20px 0" onclick="store.order('${product.id}')">Buy Now!</button>`
             : '';
         el.innerHTML = info + introPrice + discounts + subInfo + button;
     }
 }
 
+function orderDiscount(productId, discountId) {
+  const product = store.get(productId);
+  if (!store.get(store.APPLICATION)) {
+    alert('Please run store.validate() before ordering a discount.');
+    return;
+  }
+  if (!store.getApplicationUsername(product)) {
+    alert('Please make sure store.applicationUsername is set before ordering a discount.');
+    return;
+  }
+  console.log('orderDiscount("' + productId + '", "' + discountId + '")');
+  const request = {
+    appBundleID: store.get(store.APPLICATION).id,
+    productID: productId,
+    offerID: discountId,
+    applicationUsername: store.utils.md5(store.getApplicationUsername()),
+  };
+  store.utils.ajax({
+    url: 'http://localhost:3000/offer',
+    method: 'POST',
+    data: request,
+    success: function(data) {
+      if (data && data.error) {
+        errorHandler(data);
+        return;
+      }
+      // example data: {
+      //     "keyID": "D76V8WWKQ2",
+      //     "nonce": "ab7a27a7-50b6-4b81-b008-a3b1b922287c",
+      //     "timestamp": 1568976952688,
+      //     "signature": "NEYCIQC1FLmYb0PkzmpEowdcIZsVTcj09kAa1iZ8fbnJZT6hqQIhANIMLry52eDe5kMkz+n+rgnXw6D13YCLALWBw9pMDpt+"
+      // }         
+      const orderData = {
+        applicationUsername: request.applicationUsername,
+        discount: {
+          id: discountId,
+          key: data.keyID,
+          nonce: data.nonce,
+          timestamp: data.timestamp,
+          signature: data.signature,
+        },
+      };
+      console.log('order(' + productId + '): ' + JSON.stringify(orderData));
+      store.order(productId, orderData);
+    },
+    error: function(status, message, data) {
+      console.log('error: ' + JSON.stringify({status: status, message: message, data: data}));
+      // errorHandler(data);
+    },
+  });
+}
+
+function errorHandler(error) {
+  document.getElementById('error').textContent = `ERROR ${error.code}: ${error.message}`;
+  setTimeout(() => {
+    document.getElementById('error').innerHTML = '<br/>';
+  }, 10000);
+  if (error.code === store.ERR_LOAD_RECEIPTS) {
+    // Cannot load receipt, ask user to refresh purchases.
+    setTimeout(() => {
+      alert('Cannot access purchase information. Use "Refresh" to try again.');
+    }, 1);
+  }
+}
